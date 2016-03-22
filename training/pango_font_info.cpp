0473f14f2921038751ba5341661e680f3a86dab7
amitdo <amitdev2222@gmail.com>
2016-03-22 12:57:43 +0200

    pango_font_info.cpp: Fix #ifdefs for MinGW and Cygwin

--- training/pango_font_info.cpp
+++ training/pango_font_info.cpp
@@ -22,11 +22,15 @@
 #include "config_auto.h"
 #endif
 
-#if (defined MINGW) || (defined __CYGWIN__)
+#if (defined __MINGW32__) || (defined __CYGWIN__)
 // workaround for stdlib.h and putenv
 #undef __STRICT_ANSI__
+#endif
+
+#if (defined __MINGW32__)
 #include "strcasestr.h"
-#endif  // MINGW/Cygwin
+#endif
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
