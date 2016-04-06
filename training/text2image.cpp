9d052f453666a8640ea308bfa0c599144523faa8
amitdo <amitdev2222@gmail.com>
2016-04-06 11:39:48 +0300

    Check that the suggested font name is not empty

--- training/text2image.cpp
+++ training/text2image.cpp
@@ -442,10 +442,12 @@ int main(int argc, char** argv) {
   if (!FLAGS_find_fonts && !FontUtils::IsAvailableFont(FLAGS_font.c_str())) {
     string pango_name;
     if (!FontUtils::IsAvailableFont(FLAGS_font.c_str(), &pango_name)) {
-      tprintf("Could not find font named %s. Pango suggested font %s\n"
-              "Please correct '--font' arg.\n",
-              FLAGS_font.c_str(), pango_name.c_str());
-              exit(EXIT_FAILURE);
+      tprintf("Could not find font named %s.\n", FLAGS_font.c_str());
+      if (!pango_name.empty()) {
+        tprintf("Pango suggested font %s\n", pango_name.c_str());
+      }
+      tprintf("Please correct '--font' arg.\n");
+      exit(EXIT_FAILURE);
     }
   }
 
